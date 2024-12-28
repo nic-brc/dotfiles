0553f8ee7f2443aed5ab6773c3ec915058ccb335
cn669600 <christoph.niederer@broadcom.com>
2024-12-28 12:31:22 +0100

    feat(install): add check for stow command
    Added a check in install.sh to ensure that the 'stow' command is
    installed before proceeding. If 'stow' is not found, the script
    exits with an error message.

--- install.sh
+++ install.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if ! command -v stow &> /dev/null; then
+  echo "stow could not be found, please install it first."
+  exit 1
+fi
+
 for dir in */; do
   dir=${dir%/}
   if [[ $dir != .* ]]; then
--- tmux/.config/tmux/tmux-sessionizer.sh
+++ tmux/.config/tmux/tmux-sessionizer.sh
@@ -3,7 +3,7 @@
 if [[ $# -eq 1 ]]; then
     selected=$1
 else
-    selected=$(find ~/projects ~/vaults ~/.config -mindepth 1 -maxdepth 1 -type d | fzf)
+    selected=$(find ~/projects ~/vaults -mindepth 1 -maxdepth 1 -type d | fzf)
 fi
 
 if [[ -z $selected ]]; then
