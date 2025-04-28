a78a4507f20e1160aa0341b3265521cee964bbbb
nic-brc <christoph.niederer@broadcom.com>
2025-04-28 07:50:04 +0200

    chore(cleanup): Cleanup js dap config

--- nvim/.config/nvim/lua/nic/core/keymaps.lua
+++ nvim/.config/nvim/lua/nic/core/keymaps.lua
@@ -9,6 +9,10 @@ vim.keymap.set("n", "<c-j>", ":wincmd j<CR>")
 vim.keymap.set("n", "<c-h>", ":wincmd h<CR>")
 vim.keymap.set("n", "<c-l>", ":wincmd l<CR>")
 
+-- Move selection in visual mode
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
 -- Stay in visual mode when indenting
 vim.keymap.set('v', '<', '<gv')
 vim.keymap.set('v', '>', '>gv')
@@ -33,9 +37,6 @@ vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
 
 vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
-
--- Session Manager Searches
-vim.keymap.set("n", "<leader><C-s>", "<cmd>SessionSearch<CR>")
 
 -- This is going to get me cancelled
 vim.keymap.set("i", "<C-c>", "<Esc>")
--- nvim/.config/nvim/lua/nic/plugins/avante.lua
+++ nvim/.config/nvim/lua/nic/plugins/avante.lua
@@ -13,7 +13,7 @@ return {
 		provider = "gemini",
 		gemini = {
 			endpoint = "https://generativelanguage.googleapis.com/v1beta/models",
-			model = "gemini-2.5-pro-exp-03-25",
+			model = "gemini-2.0-flash",
 			timeout = 30000,
 			temperature = 0,
 			max_tokens = 4096,
