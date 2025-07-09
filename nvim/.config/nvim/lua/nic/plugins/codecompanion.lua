f03ef6104e97799415d144707a44a5db425ae322
nic-brc <christoph.niederer@broadcom.com>
2025-07-09 08:40:58 +0200

    feat(codecompanion): Add custom prompt library and keymaps
    This commit introduces a custom prompt library configuration for
    CodeCompanion.nvim, allowing for user-defined prompts.
    
    Additionally, it adds new keybindings:
    - `<leader>ah` to open CodeCompanion actions.
    - `<leader>ac` to toggle the CodeCompanion chat window.

--- nvim/.config/nvim/lua/nic/plugins/codecompanion.lua
+++ nvim/.config/nvim/lua/nic/plugins/codecompanion.lua
@@ -35,6 +35,40 @@ return {
 			opts = {
 				log_level = "DEBUG",
 			},
+			prompt_library = {
+				["My New Prompt"] = {
+					strategy = "chat",
+					description = "Some cool custom prompt you can do",
+					prompts = {
+						{
+							role = "system",
+							content = "You are an experienced developer with Lua and Neovim",
+						},
+						{
+							role = "user",
+							content = "Can you explain why ...",
+						},
+					},
+				},
+			},
+		},
+		keys = {
+			{
+				"<leader>ah",
+				"<cmd>CodeCompanionActions<cr>",
+				mode = { "n", "v" },
+				noremap = true,
+				silent = true,
+				desc = "CodeCompanion actions",
+			},
+			{
+				"<leader>ac",
+				"<cmd>CodeCompanionChat Toggle<cr>",
+				mode = { "n", "v" },
+				noremap = true,
+				silent = true,
+				desc = "CodeCompanion chat",
+			},
 		},
 	},
 }
