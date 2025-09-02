e2f0f1261f45e48ee92e1ef444c6ca71aee28b62
nic-brc <christoph.niederer@broadcom.com>
2025-09-02 09:02:24 +0200

    feat(add): Open blink

--- nvim/.config/nvim/lua/nic/plugins/lsp/lspconfig.lua
+++ nvim/.config/nvim/lua/nic/plugins/lsp/lspconfig.lua
@@ -64,19 +64,11 @@ return {
 					map("gI", require("telescope.builtin").lsp_implementations, "[G]oto [I]mplementation")
 					map("<leader>D", require("telescope.builtin").lsp_type_definitions, "Type [D]efinition")
 					map("<leader>ds", require("telescope.builtin").lsp_document_symbols, "[D]ocument [S]ymbols")
-
-					map(
-						"<leader>ws",
-						require("telescope.builtin").lsp_dynamic_workspace_symbols,
-						"[W]orkspace [S]ymbols"
-					)
+					map("<leader>ws", require("telescope.builtin").lsp_dynamic_workspace_symbols, "[W]orkspace [S]ymbols")
 
 					map("<leader>wd", require("telescope.builtin").diagnostics, "Show Workspace diagnostics")
-
 					map("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
-
 					map("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction", { "n", "x" })
-
 					map("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
 
 					-- The following two autocommands are used to highlight references of the
