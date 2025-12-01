317be26913ddb66c712aa79b84b90d282b409e32
nic-brc <christoph.niederer@broadcom.com>
2025-12-01 07:48:36 +0100

    chore(replace): telescope with snacks

--- nvim/.config/nvim/lua/nic/plugins/lsp/lspconfig.lua
+++ nvim/.config/nvim/lua/nic/plugins/lsp/lspconfig.lua
@@ -51,26 +51,6 @@ return {
 			vim.api.nvim_create_autocmd("LspAttach", {
 				group = vim.api.nvim_create_augroup("UserLspConfig", {}),
 				callback = function(event)
-					local map = function(keys, func, desc, mode)
-						mode = mode or "n"
-						vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
-					end
-
-					map("gd", require("telescope.builtin").lsp_definitions, "[G]oto [D]efinition")
-					-- map("gd", vim.lsp.buf.definition, "[G]oto [D]efinition")
-					-- map("<leader>D", vim.lsp.buf.lsp_type_definitions, "Type [D]efinition")
-
-					map("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
-					map("gI", require("telescope.builtin").lsp_implementations, "[G]oto [I]mplementation")
-					map("<leader>D", require("telescope.builtin").lsp_type_definitions, "Type [D]efinition")
-					map("<leader>ds", require("telescope.builtin").lsp_document_symbols, "[D]ocument [S]ymbols")
-					map("<leader>ws", require("telescope.builtin").lsp_dynamic_workspace_symbols, "[W]orkspace [S]ymbols")
-
-					map("<leader>wd", require("telescope.builtin").diagnostics, "Show Workspace diagnostics")
-					map("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
-					map("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction", { "n", "x" })
-					map("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
-
 					-- The following two autocommands are used to highlight references of the
 					-- word under your cursor when your cursor rests there for a little while.
 					--    See `:help CursorHold` for information about when this is executed
@@ -79,15 +59,6 @@ return {
 					local client = vim.lsp.get_client_by_id(event.data.client_id)
 					if client then
 						client.server_capabilities.semanticTokensProvider = nil
-					end
-					-- The following code creates a keymap to toggle inlay hints in your
-					-- code, if the language server you are using supports them
-					--
-					-- This may be unwanted, since they displace some of your code
-					if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint) then
-						map("<leader>th", function()
-							vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({ bufnr = event.buf }))
-						end, "[T]oggle Inlay [H]ints")
 					end
 				end,
 			})
